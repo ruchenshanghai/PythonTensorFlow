0c0354769b52b3ee3b9439a5c68afeefb0f7e764
濡尘赏海 <996956441@qq.com>
2017-11-01 22:21:07 +0800

    initial

--- PythonTensorFlow/TestImport.py
+++ PythonTensorFlow/TestImport.py
@@ -1,4 +1,5 @@
 import os
+import re
 from PIL import Image
 import numpy
 
@@ -7,13 +8,18 @@ class TestImport(object):
     def __init__(self):
         self.datas = []
     def initialByImage(self, dir):
+        nameList = []
         for filename in os.listdir(dir):
+            nameList.append(filename)
+        newList = sorted(nameList,key = lambda i:int(re.match(r'(\d+)',i).group()))
+        #print(newList)
+        for filename in nameList:
                 imgPath = os.path.join(dir, filename)
                 img = Image.open(imgPath)
                 img = img.convert('L').resize((28,28))
                 width, height = img.size
                 img = numpy.asarray(img, dtype='float64') / 256
-                temp = img.reshape(1, height * width)[0]
+                temp = img.reshape(1, 28 * 28)[0]
                 self.datas.append(temp)
         self.dataSize = len(self.datas)
 
